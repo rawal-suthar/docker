a0aa444a73852a10716f05e93c1ac4261cdaa5e4
Rawal Suthar <rawal.suthar@consectus.com>
2025-02-05 11:19:57 +0530

    FIX: Optimisation

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ db.connect(err => {
 
 // Route to get database server details
 app.get('/db-info', (req, res) => {
+    console.log({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    });
     db.query('SELECT VERSION() AS version, DATABASE() AS database_name, USER() AS user', (err, results) => {
         if (err) return res.status(500).send(err);
         res.json(results[0]);
